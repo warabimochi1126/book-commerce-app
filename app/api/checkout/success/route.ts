cd295e689db8dc1fad9f2e5c404da0319e341a25
warabimochi <satomi920003@yahoo.co.jp>
2024-02-15 01:27:14 +0900

    sync

---
+++ app/api/checkout/success/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+// 購入履歴の保存
+
+export async function POST(request: Request, response: Response) {
+    try {
+
+    } catch(err) {
+        return NextResponse.json(err);
+    }
+}
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,14 @@
 "use client";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Header = () => {
+  const { data: session } = useSession();
+  const user = session?.user;
+  console.log(user);
+
   return (
     <header className="bg-slate-600 text-gray-100 shadow-lg">
       <nav className="flex items-center justify-between p-4">
@@ -19,18 +24,22 @@ const Header = () => {
             ホーム
           </Link>
           <Link
-            href="/login"
+            href={user ?  "/profile" : "/login"}
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
-            ログイン
+            {user ? "プロフィール" : "ログイン"}
           </Link>
+          { user ? (
+            <button onClick={() => signOut({ callbackUrl: "/login" })} className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >ログアウト</button>
+          ) : ("")}
 
           <Link href={`/profile`}>
             <Image
               width={50}
               height={50}
               alt="profile_icon"
-              src={"/default_icon.png"}
+              src={user?.image || "/default_icon.png"}
             />
           </Link>
         </div>
