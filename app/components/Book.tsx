7fdbe9d52fb554f64eb976bf5a46aa264b2454c5
warabimochi <satomi920003@yahoo.co.jp>
2024-02-18 00:52:28 +0900

    done

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -59,7 +59,7 @@ const Book = ({ book, isPurchased }: BookProps) => {
     if(!user) {
       setShowModal(false);
       // ログインページへリダイレクト
-      router.push("/login");
+      router.push("/api/auth/signin");
     } else {
       // Stripeで決済する。
       startCheckout();
